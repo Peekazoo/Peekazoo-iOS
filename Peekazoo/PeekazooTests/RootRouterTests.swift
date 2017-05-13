8a05a32e232d40ef9da5b52b4883d6a4228c958e
ShezHsky <shezhsky@icloud.com>
2017-05-13 08:54:44 +0100

    Tidy up root router tests

--- Peekazoo/PeekazooTests/RootRouterTests.swift
+++ Peekazoo/PeekazooTests/RootRouterTests.swift
@@ -11,35 +11,33 @@ import XCTest
 
 class RootRouterTests: XCTestCase {
     
-    func testSetNavigationControllerAsTheRootViewController() {
-        let window = UIWindow()
-        let router = RootRouter(window: window)
-        _ = router.navigateToRoot()
+    var window: UIWindow!
+    var router: RootRouter!
+    
+    override func setUp() {
+        super.setUp()
         
+        window = UIWindow()
+        router = RootRouter(window: window)
+    }
+    
+    func testSetNavigationControllerAsTheRootViewController() {
+        _ = router.navigateToRoot()
         XCTAssertTrue(window.rootViewController is UINavigationController)
     }
     
     func testSetTheHomepageViewControllerAsTheTopViewControllerOnTheRoot() {
-        let window = UIWindow()
-        let router = RootRouter(window: window)
         _ = router.navigateToRoot()
-        
         XCTAssertTrue((window.rootViewController as? UINavigationController)?.topViewController is HomepageViewController)
     }
     
     func testReturnTheHomepageViewController() {
-        let window = UIWindow()
-        let router = RootRouter(window: window)
         let presented = router.navigateToRoot().viewController
-        
         XCTAssertEqual(presented, (window.rootViewController as? UINavigationController)?.topViewController)
     }
     
     func testReturnTheHomepageRouter() {
-        let window = UIWindow()
-        let router = RootRouter(window: window)
         let homepageRouter = router.navigateToRoot().router
-        
         XCTAssertTrue(homepageRouter is HomepageRouter)
     }
     
