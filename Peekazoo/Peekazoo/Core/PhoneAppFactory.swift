43669badeee9a9074e21cac816ee065197f8dbf0
ShezHsky <shezhsky@icloud.com>
2017-05-15 22:10:24 +0100

    Add basic implementation of service for manual testing

--- Peekazoo/Peekazoo/Core/PhoneAppFactory.swift
+++ Peekazoo/Peekazoo/Core/PhoneAppFactory.swift
@@ -11,12 +11,25 @@ import UIKit
 struct PhoneAppFactory: AppFactory {
 
     func makeApplication(window: UIWindow) -> App {
-        class DummyHomepageService: HomepageService {
-            func loadHomepage(delegate: HomepageServiceLoadingDelegate) {}
+        class ManualTestingHomepageService: HomepageService {
+            struct StubItem: HomepageItem {
+                var contentIdentifier: String
+                var title: String
+            }
+
+            func loadHomepage(delegate: HomepageServiceLoadingDelegate) {
+                let threeSecondsFromNow = DispatchTime(uptimeNanoseconds: DispatchTime.now().rawValue + (NSEC_PER_SEC * 3))
+                DispatchQueue.main.asyncAfter(deadline: threeSecondsFromNow) {
+                    let items = [StubItem(contentIdentifier: "1", title: "First"),
+                                 StubItem(contentIdentifier: "2", title: "Second"),
+                                 StubItem(contentIdentifier: "3", title: "Third")]
+                    delegate.homepageServiceDidLoadSuccessfully(content: items)
+                }
+            }
         }
 
         return PhoneApp(rootRouter: WindowRootRouter(window: window),
-                        homepageService: DummyHomepageService())
+                        homepageService: ManualTestingHomepageService())
     }
 
 }
