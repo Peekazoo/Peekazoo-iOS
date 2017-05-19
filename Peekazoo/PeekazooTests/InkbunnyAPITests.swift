cf6c21fd17b197cecf36a3530cbad96d81f0552b
ShezHsky <shezhsky@icloud.com>
2017-05-19 21:49:01 +0100

    Request search endpoint when login succeeds

--- Peekazoo/PeekazooTests/InkbunnyAPITests.swift
+++ Peekazoo/PeekazooTests/InkbunnyAPITests.swift
@@ -23,7 +23,11 @@ struct InkbunnyAPI {
 
     func loadHomepage(completionHandler: @escaping (InkbunnyHomepageLoadResult) -> Void) {
         let loginURL = URL(string: "https://inkbunny.net/api_login.php")!
-        networkAdapter.get(loginURL) { _, _ in completionHandler(.failure) }
+        networkAdapter.get(loginURL) { _, _ in
+            completionHandler(.failure)
+            let searchURL = URL(string: "https://inkbunny.net/api_search.php")!
+            self.networkAdapter.get(searchURL, completionHandler: { _, _ in })
+        }
     }
 
 }
@@ -36,6 +40,22 @@ class CapturingInkbunnyHomepageHandler {
         case .failure:
             wasNotifiedFeedDidFailToLoad = true
         }
+    }
+
+}
+
+class JournallingNetworkAdapter: NetworkAdapter {
+
+    var networkAdapter: NetworkAdapter
+
+    init(networkAdapter: NetworkAdapter) {
+        self.networkAdapter = networkAdapter
+    }
+
+    private(set) var getURLs = [URL]()
+    func get(_ url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        getURLs.append(url)
+        networkAdapter.get(url, completionHandler: completionHandler)
     }
 
 }
@@ -67,6 +87,17 @@ class InkbunnyAPITests: XCTestCase {
         inkbunnyAPI.loadHomepage(completionHandler: capturingHomepageHandler.verify)
 
         XCTAssertFalse(capturingHomepageHandler.wasNotifiedFeedDidFailToLoad)
+    }
+
+    func testLoginRequestReturnsValidResponseRequestsSearchEndpoint() {
+        let expectedSearchEndpoint = URL(string: "https://inkbunny.net/api_search.php")!
+        let successfulNetworkAdapter = SuccessfulNetworkAdapter(contentsOfJSONFile: "ValidInkbunnyGuestLoginResponse")
+        let journallingNetworkAdapter = JournallingNetworkAdapter(networkAdapter: successfulNetworkAdapter)
+        let inkbunnyAPI = InkbunnyAPI(networkAdapter: journallingNetworkAdapter)
+        let capturingHomepageHandler = CapturingInkbunnyHomepageHandler()
+        inkbunnyAPI.loadHomepage(completionHandler: capturingHomepageHandler.verify)
+
+        XCTAssertEqual(true, journallingNetworkAdapter.getURLs.last?.absoluteString.contains(expectedSearchEndpoint.absoluteString))
     }
 
 }
